3b4cdab6c5f643a990988085c82659922b3e7fac
Shaxboz <sh.aliqulov@tuit.uz>
2025-04-28 17:22:52 +0500

    uploaded home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:habit_tracker/authentication/signup_page.dart';
+=======
+import 'package:habit_tracker/home_page.dart';
+>>>>>>> Stashed changes
 
 void main() {
   runApp(const MainApp());
@@ -12,11 +16,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+<<<<<<< Updated upstream
       home: Scaffold(
         body: Center(
           child: SignupPage(),
         ),
       ),
+=======
+      home: HomePage(),
+>>>>>>> Stashed changes
     );
   }
 }
